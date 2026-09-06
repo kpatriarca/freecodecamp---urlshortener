bcc2eac8a8dabe2827a6c8f8682be30c40caf916
kpatriarca <kestpatriarca18@gmail.com>
2026-09-06 17:46:30 +0800

    commit

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const dns = require("dns");
 const { URL } = require("url");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/public", express.static(`${process.cwd()}/public`));
